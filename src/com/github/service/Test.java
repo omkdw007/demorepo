81fe773e00e50a4f3ebd5bf8fc4574df85a4e9c1
omkar <omkarwadkar@gmail.com>
2013-02-18 01:07:59 -0500

    made changes to Test

--- src/com/github/service/Test.java
+++ src/com/github/service/Test.java
@@ -1,13 +1,21 @@
 package com.github.service;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Test {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+	public static void main(String[] args) { 
+    String    [] names = {"omkar","amit","darshan","Omkar"};
+    System.out.println(Arrays.asList(names));
+ 
+    Arrays.sort(names);
+   
+    System.out.println(Arrays.asList(names));
+    
 	}
 
 }
